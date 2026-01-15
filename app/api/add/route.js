58a43cbd09f96125dced3723760e3c580bd07308
pjha91275 <pjha91275@gmail.com>
2026-01-15 14:46:27 +0530

    Changed route.js and mongodb.js for deployment

--- app/api/add/route.js
+++ app/api/add/route.js
@@ -1,20 +1,30 @@
-import clientPromise from '@/lib/mongodb'
+import { NextResponse } from "next/server"
+import clientPromise from "@/lib/mongodb"
 
 export async function POST(request) {
+  const body = await request.json()
 
-    const body = await request.json()
-    const client = await clientPromise;
-    const db = client.db("Linktree")
-    const collection = db.collection("links")
+  const client = await clientPromise
+  const db = client.db("Linktree")
+  const collection = db.collection("links")
 
-    // If the handle is already claimed, you cannot create the Linktree
-    const doc = await collection.findOne({handle: body.handle})
+  const doc = await collection.findOne({ handle: body.handle })
 
-    if(doc){
-        return Response.json({ success: false, error: true, message: 'This Linktree alrady exoists!', result: null})
-    }
+  if (doc) {
+    return NextResponse.json({
+      success: false,
+      error: true,
+      message: "This Linktree already exists!",
+      result: null,
+    })
+  }
 
-    const result = await collection.insertOne(body)
+  const result = await collection.insertOne(body)
 
-    return Response.json({ success: true, error: false, message: 'Your Linktree has been generated!', result: result,})
+  return NextResponse.json({
+    success: true,
+    error: false,
+    message: "Your Linktree has been generated!",
+    result,
+  })
 }
